4f86dcbf53e834822d297ed7329e09d953e4f5e9
Marcus Connolly <mnjconnolly@gmail.com>
2023-10-07 10:41:57 -0400

    feat: update prisma schema and add seed dev data

---
+++ prisma/seed.ts
@@ -0,0 +1,72 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+	console.log('Seeding beginning');
+	const adminRole = await prisma.role.upsert({
+		where: { name: 'admin' },
+		create: { name: 'admin' },
+		update: {}
+	});
+	const clientRole = await prisma.role.upsert({
+		where: { name: 'client' },
+		create: { name: 'client' },
+		update: {}
+	});
+
+	const coachRole = await prisma.role.upsert({
+		where: { name: 'coach' },
+		create: { name: 'coach' },
+		update: {}
+	});
+
+	const marcus = await prisma.user.upsert({
+		where: { email: 'mconnolly@gmail.com' },
+		update: {},
+		create: {
+			email: 'mconnolly@gmail.com',
+			firstName: 'Marcus',
+			lastName: 'Connolly',
+			roles: {
+				create: [
+					{
+						roleName: 'admin'
+					},
+					{ roleName: 'coach' }
+				]
+			},
+			Coach: {
+				create: {
+					hourlyRateInCents: 60_000
+				}
+			}
+		}
+	});
+	const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+	const skaters: {
+		id: string;
+		firstName: string;
+		lastName: string;
+		email: string;
+		dateOfBirth: Date | null;
+		createdOn: Date;
+		modifiedOn: Date | null;
+	}[] = [];
+	for (const num of numbers) {
+		const skater = await prisma.skater.upsert({
+			where: { firstName_lastName: { firstName: 'Skater', lastName: num } },
+			update: {},
+			create: {
+				firstName: 'Skater',
+				lastName: num,
+				email: `skater_${num.toLowerCase()}@gmail.com`
+			}
+		});
+		skaters.push(skater);
+	}
+
+	console.log({ marcus, adminRole, clientRole, coachRole, skaters });
+	console.log('Seeding complete');
+}
+
+await main();
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,66 +1,14 @@
-import { randomUUID } from 'node:crypto';
-import knex from 'knex';
-const db = knex({
-	client: 'better-sqlite3', // or 'better-sqlite3'
-	connection: {
-		filename: './mydb.sqlite'
-	}
-});
-
-export type Skater = {
-	id: string;
-	firstName: string;
-	lastName: string;
-};
-
-export type Lesson = {
-	id: string;
-	coachId: string;
-	skaterIds: string[];
-	lessonTimeInMinutes: number;
-	lessonDate: Date;
-	createdDate: Date;
-	modifiedDate: Date | null;
-};
-
-const skaterMap: Map<string, Skater> = new Map([
-	['1', { id: '1', firstName: 'James', lastName: 'Connolly' }],
-	['2', { id: '2', firstName: 'Marcus', lastName: 'Connolly' }],
-	['3', { id: '3', firstName: 'Laurence', lastName: 'Lessard' }]
-]);
-
-const lessons = [];
-
-export function addLesson(
-	coachId: string,
-	skaterIds: string[],
-	lessonTimeInMinutes: number,
-	lessonDate: Date
-) {
-	const newLesson: Lesson = {
-		id: randomUUID(),
-		coachId,
-		skaterIds,
-		lessonTimeInMinutes,
-		lessonDate,
-		createdDate: new Date(),
-		modifiedDate: null
-	};
-	lessons.push(newLesson);
-	return newLesson;
-}
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
 
 export function getSkaters() {
-	return [...skaterMap.values()];
+	return prisma.skater.findMany();
 }
 
 export function getSkaterById(id: string) {
-	return skaterMap.get(id);
+	return prisma.skater.findUnique({ where: { id } });
 }
 
-export function addSkater(firstName: string, lastName: string) {
-	const id = randomUUID();
-	const newSkater = { id, firstName, lastName };
-	skaterMap.set(id, newSkater);
-	return newSkater;
+export async function addSkater(firstName: string, lastName: string, email: string) {
+	return prisma.skater.create({ data: { firstName, lastName, email } });
 }

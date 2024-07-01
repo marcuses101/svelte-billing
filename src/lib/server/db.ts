808967e1f2cfb92093b3163e6726bee175042c51
Marcus Connolly <mnjconnolly@gmail.com>
2024-07-01 15:18:20 -0400

    fix: hacky prisma issue fix
    https://github.com/prisma/prisma/issues/15614

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './prisma';
 import type { Prisma } from '@prisma/client';
 import { ACCOUNT_TYPE_CODE, type SkaterType } from '../defs';
 
---
+++ src/lib/server/prisma.ts
@@ -0,0 +1,11 @@
+// this is a hack pending https://github.com/prisma/prisma/issues/15614
+
+import { PrismaClient as ImportedPrismaClient } from '@prisma/client';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const { PrismaClient: RequiredPrismaClient } = require('@prisma/client');
+const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient;
+
+export class PrismaClient extends _PrismaClient {}
